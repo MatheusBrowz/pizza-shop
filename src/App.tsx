ad222d6de4979b1e781a06e1c41a3e117bee83d6
Matheus Lima <matheuslimaworks@gmail.com>
2025-04-06 10:01:27 -0300

    add axios and react-query

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,18 @@ import { router } from "./routes";
 
 import { Toaster } from "sonner";
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 export function App() {
   return (
     <>
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
         <title>| pizza shop</title>
-        <RouterProvider router={router} />
         <Toaster richColors />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
